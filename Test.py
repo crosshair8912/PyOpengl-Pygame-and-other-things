bbf9b51a641dcc1ebe92e27c0abd42206843e808
crosshair8912 <asvovsem@gmail.com>
2016-10-15 09:00:31 +0300

    Added some new features

--- Test.py
+++ Test.py
@@ -3,45 +3,55 @@ import OpenGL.GL as GL
 import OpenGL.GLU as GLU
 import OpenGL.GLUT as GLUT
 import obj_parser
+
+tmp = obj_parser.parse("crate.obj","v")
+tmp_1 = obj_parser.parse("untitled.obj","v")
+lol = 0
+
+vertex_x_array = []
+vertex_y_array = []
+vertex_z_array = []
+cnt = 0
+for i in tmp:
+	cnt += 1
+	for j in i:
+		if cnt == 1:
+			vertex_x_array.append(j)
+		elif cnt == 2:
+			vertex_y_array.append(j)
+		elif cnt == 3:
+			vertex_z_array.append(j)
+
 def init():
 	GL.glClearColor(0.25,0.25,0.25,1)
-	#GL.glClearColor(0.7, 0.8, 0.7, 1.0)
-	GLU.gluOrtho2D(-10.0,10.0,-10.0,10.0)
+	GLU.gluOrtho2D(-5.0,5.0,-5.0,5.0)
 	
-# def plotpoints():
-	# GL.glClear(GL.GL_COLOR_BUFFER_BIT)
-	# GL.glColor3f(1.0,1.0,1.0)
-	# #GL.glLineWidth(3.0)
-	# #GL.glPointSize(1.0)
-	# GL.glBegin(GL.GL_LINE_LOOP)
-	# GL.glVertex3f(0.0,1.0,0.0)
-	# GL.glVertex3f(1.0,0.0,0.0)
-	# GL.glVertex3f(-1.0,0.0,0.0)
-	# GL.glVertex3f(0.0,-1.0,0.0)
-	# GL.glEnd()
-	# GL.glFlush()
+	
 def plotpoints():
 	GL.glClear(GL.GL_COLOR_BUFFER_BIT)
-	GL.glColor3f(0.0, 0.0, 0.0)
+	GL.glColor3f(1.0, 1.0, 1.0)
+	
 	GL.glBegin(GL.GL_LINE_LOOP)
-	tmp = obj_parser.parse("crate.obj","v")
-	vertex_x_array = []
-	vertex_y_array = []
-	vertex_z_array = []
-	cnt = 0
-	for i in tmp:
-		cnt += 1
-		for j in i:
-			if cnt == 1:
-				vertex_x_array.append(j)
-			elif cnt == 2:
-				vertex_y_array.append(j)
-			elif cnt == 3:
-				vertex_z_array.append(j)
 	for i,j,k in zip(vertex_x_array,vertex_y_array,vertex_z_array):
 		GL.glVertex3f(i,j,k)
 	GL.glEnd()
+	GL.glBegin(GL.GL_LINE_LOOP)
+	for i,j,k in zip(vertex_y_array,vertex_x_array,vertex_z_array):
+		GL.glVertex3f(i,j,k)
+	GL.glEnd()
+	GLU.gluLookAt(0.0,0.0,0.0,vertex_x_array[0]+lol,vertex_y_array[0]+lol,vertex_z_array[0]+lol,1,1,1)
 	GL.glFlush() 
+def keyPressed(a,b,c):
+	global lol
+	if a == '\xf4':
+		lol +=1
+		GLUT.glutPostRedisplay()
+		lol = 0
+	if a == '\xe2':
+		lol -=1
+		GLUT.glutPostRedisplay()
+		lol = 0
+	print(lol)
 def main():
 	
 	GLUT.glutInit(sys.argv)
@@ -49,8 +59,9 @@ def main():
 	GLUT.glutInitWindowSize(500,500)
 	GLUT.glutInitWindowPosition(50,50)
 	GLUT.glutCreateWindow("Plot points")
-	GLUT.glutDisplayFunc(plotpoints)
 	
 	init()
+	GLUT.glutKeyboardFunc(keyPressed)
+	GLUT.glutDisplayFunc(plotpoints)
 	GLUT.glutMainLoop()
-main(main())
+main()
